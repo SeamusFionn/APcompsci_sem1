e7f3f9a12d922da753625e81f38d6ab5c5eb133b
Oconnell_Seamus <Seamusfionn@gmail.com>
2016-12-14 12:09:45 -0800

    improved my lesson 10 stuffs

--- APLesson_010/Compos.java
+++ APLesson_010/Compos.java
@@ -6,8 +6,37 @@ public class Compos
 	public static void main(String[]args)
 	{
 		Integer [] numbers = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
-		ArrayList<Integer> nums = new ArrayList<>(Arrays.aslist(numbers));
+		ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(numbers));
 		
 		System.out.println(nums);
+	}
+	
+	public static int gFactor(int n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (n % i == 0)
+			{
+				return 1;
+			}
+			
+			else
+			{
+				return 0;
+			}
+		}
+	}
+	
+	public static int removePrimes(ArrayList(nums))
+	{
+		for (int i = 0; i < nums.length; i++)
+		{
+			if (gFactor(nums.get(i) == 0))
+			{
+				nums.remove(i);
+				i -= 1;
+			}
+		}
+		
 	}
 }
---
+++ APLesson_010/expsolve.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class expsolve
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("please enter an equation: ");
+		String expression = kb.nextLine();
+		
+		ArrayList<String>equation = new ArrayList<>(Arrays.asList(expression.split("")));
+	
+	}
+	
+	public static void doEquation(ArrayList)
+	{
+		int i = 0;
+		while(i < equation.size())
+		{
+			if(i < equation.size() && equation.get(i).equals("+"))
+			{
+				equation.set(i, "" + (Integer.parseInt(equation.get(i - 1)) + Integer.parseInt(equation.get(i + 1))));
+				equation.remove(i - 1);
+				equation.remove(i);
+			}
+			else
+			{
+				i++;
+			}
+		}
+		
+		System.out.println(equation);
+	}
+}
