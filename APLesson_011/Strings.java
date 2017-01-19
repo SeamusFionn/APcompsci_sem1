f591f48356003899d16da80984f3dac1e389b318
Oconnell_Seamus <Seamusfionn@gmail.com>
2017-01-19 11:25:42 -0800

    changed strings a little

--- APLesson_011/Strings.java
+++ APLesson_011/Strings.java
@@ -4,14 +4,18 @@ public class Strings
 {
 	public static void main(String[]args)
 	{
+	//	Scanner kb = new Scanner(System.in);
 		String[][]words = new String [4][4];
 		
 		for(int i = 0; i < words.length; i++)
 		{
 			for(int j = 0; j < words[i].length; j++)
 			{
+		        Scanner kb = new Scanner(System.in);
 				System.out.println("Please enter a word");
-				
+				i = kb.nextInt();
+				System.out.println("Please enter a word");
+				j = kb.nextInt();
 			}
 		}
 		
@@ -29,5 +33,3 @@ public class Strings
 
 
 
-//(int)(Math.random()*50 + 1);
-//letters.substring(spot, spot + 1);
